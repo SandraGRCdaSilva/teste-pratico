c6702403f1b0169b73a53647ff06e8013e1f351a
SandraGRCdaSilva <34241175+SandraGRCdaSilva@users.noreply.github.com>
2020-01-11 15:42:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Teste Prático ECONVERSE</title>
+
+<link rel="stylesheet" href="css/gallery.theme.css">
+<link rel="stylesheet" href="css/gallery.min.css">
+<link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet/less" type="text/css" href="styles.less" />
+
+</head>
+<body>
+<div class="sidebar">
+    <center>
+            <img class="barra" src="imagens/password.svg">
+            <span class="label1">COMPRA <b>100% SEGURA</b></span>
+        </a>
+        <a class="barra"> |</a>
+        <a class="focos">
+            <img class="barra" src="imagens/percentage.svg" style="height: 15px; width: 15px;">
+            <span class="label1"><b>5% OFF</b> NO CARTÃO DE CRÉDITO</span>
+        </a>
+        <a class="barra"> |</a>
+        <a class="focos">
+            <img class="barra" src="imagens/truck.svg" style="height: 20px; width: 30px;">
+            <span class="label1">ENTREGA EM <b>24 HORAS</b></span>
+        </a>
+    </center>
+    </div>
+</div>
+
+
+<img class="logo" src="imagens/CF-Logo-300x86.png.pagespeed.ce.nvszrGBJW1.png">
+<div class="divBusca">
+  <input type="text" id="txtBusca" class="txtBusca" placeholder="O QUE VOCÊ ESTÁ BUSCANDO"/>
+  <img src="imagens/search.svg" id="btnBusca" alt="Buscar" style="height:20px;width:20px;margin-top:5px;"/>
+</div>
+<a>
+<img class="barra" src="imagens/user.svg">
+<span>
+    OLÁ, VISITANTE &#013
+    Minha Conta
+</span>
+</a>
+<a>
+<img class="barra" src="imagens/shopping-cart.svg">
+</a>
+
+
+<div class="menu">
+    <center>
+        <a class="menu" >
+            <span class="label2">BEBIDAS</span>
+        </a>
+        <a class="menu" >
+            <span class="label2">SOBREMESAS</span>
+        </a>
+        <a class="menu" >
+            <span class="label2">CARNES</span>
+        </a>
+        <a class="menu" >
+            <span class="label2">CONGELADOS</span>
+        </a>
+        <a class="menu" >
+            <span class="label2">CONSERVAS</span>
+        </a>
+        <a class="menu" >
+            <span class="label2">TEMPEROS</span>
+        </a>
+        <a class="menu" >
+            <span class="label2">APERITIVOS</span>
+        </a>
+        <a class="menu" >
+            <span class="label2">ORGÂNICO</span>
+        </a>
+        <a class="menu" >
+            <span class="label2">OFERTAS</span>
+        </a>
+    </center>
+</div>
+<div class="gallery autoplay items-3">
+  <div id="item-1" class="control-operator"></div>
+  <div id="item-2" class="control-operator"></div>
+  <div id="item-3" class="control-operator"></div>
+  <div id="item-4" class="control-operator"></div>
+  <div id="item-5" class="control-operator"></div>
+
+
+   <figure class="item">
+     <h1></h1>
+    </figure>
+    
+    <figure class="item">
+      <h1 style="color: White;margin-top: 80px;font-family: arial;">
+      PREÇO SE DISCUTE<br>
+      <b>QUALIDADE NÃO</b>
+    </h1>
+    <h5 style="color: White;">MAIS DE 600 PRODUTOS</h5>
+    <button class="trans">Confira</button>
+    </figure>
+    
+    <figure class="item">
+      <h1></h1>
+    </figure>
+
+    <figure class="item">
+      <h1></h1>
+    </figure>
+    
+    <figure class="item">
+      <h1></h1>
+    </figure>
+    
+    <div class="controls">
+      <a href="#item-1" class="control-button">.</a>
+      <a href="#item-2" class="control-button">.</a>
+      <a href="#item-3" class="control-button">.</a>
+      <a href="#item-4" class="control-button">.</a>
+      <a href="#item-5" class="control-button">.</a>
+    </div>
+</div>
+
+<div class="banner">
+    <figure class="coca" >
+        <h1>
+        ENTREGA EM ATÉ<br>
+        <b>24 HORAS</b>
+      </h1>
+      <button class="trans">SAIBA MAIS</button>
+    </figure>
+
+    <figure class="refri" >
+        <h1>
+        REFRIGERANTE<br>
+        <b>ORGÂNICO<br>
+        & NATURAL
+        </b>
+      </h1>
+      <button class="trans">CONFIRA</button>
+    </figure>
+
+    <figure class="organico" >
+        <h1>
+        PRODUTOS<br>
+        <b>ORGÂNICOS</b>
+      </h1>
+      <button class="trans">CONFIRA</button>
+    </figure>
+
+    <figure class="sobre" >
+        <h1>
+        AS MELHORES<br>
+        <b>SOBREMESAS</b>
+      </h1>
+      <button class="trans">CONFIRA</button>
+    </figure>
+    
+</div>
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+<div class="vendidos">
+  <figure class="ovo">
+    <img class="ovo" src="imagens/7897309401453-802x1024.png">
+        <h1>
+        Ovomaltine Creme<br>
+        Crocante 660g<br>
+        <strike>R$ 147,39</strike><br>
+        <b>R$ 99,00</b>
+      </h1>
+      <button class="compra">COMPRAR</button>
+    </figure>
+
+    <figure class="ovo">
+      <img class="ovo" src="imagens/7897309401453-802x1024.png">
+        <h1>
+        Ovomaltine Creme<br>
+        Crocante 660g<br>
+        <strike>R$ 147,39</strike><br>
+        <b>R$ 99,00</b>
+      </h1>
+      <button class="compra">COMPRAR</button>
+    </figure>
+
+    <figure class="ovo">
+      <img class="ovo" src="imagens/7897309401453-802x1024.png">
+        <h1>
+        Ovomaltine Creme<br>
+        Crocante 660g<br>
+        <strike>R$ 147,39</strike><br>
+        <b>R$ 99,00</b>
+      </h1>
+      <button class="compra">COMPRAR</button>
+    </figure>
+
+    <figure class="ovo">
+  <img class="ovo" src="imagens/7897309401453-802x1024.png">
+        <h1>
+        Ovomaltine Creme<br>
+        Crocante 660g<br>
+        <strike>R$ 147,39</strike><br>
+        <b>R$ 99,00</b>
+      </h1>
+      <button class="compra">COMPRAR</button>
+    </figure>
+</div>
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+<br><br><br><br><br>
+<div class="ofertas">
+</div>
+<div class="lancamentos">
+</div>
+<div class="insta">
+</div>
+<div class="sobre">
+    <center>
+    <h2>SOBRE A <b>CENTRAL FOODS</b></h2>
+    <p style= "width:70%;">
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem 
+    ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+    magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea 
+    takimata sanctus est Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
+    invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
+    kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam 
+    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et 
+    ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur 
+    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+    </p>
+</center>
+</div>
+<div class="footer">
+
+</div>
+</body>
+</html>
